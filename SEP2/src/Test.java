e3c1e14729caae204f86efb78b73090488026a68
AnneLindberg <aans@live.dk>
2020-04-02 15:31:02 +0200

    wrote a comment for you

--- SEP2/src/Test.java
+++ SEP2/src/Test.java
@@ -5,6 +5,7 @@ public class Test
     System.out.println("Nej dig");
     System.out.println("Uno reverse");
     System.out.println("Kevin was here");
+    System.out.println("hej hilsen anne ");
   }
 
 }
