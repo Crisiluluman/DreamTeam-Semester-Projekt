62d6660427ee2f983356c7f1a0b993dde14111b3
martinb264 <martinbuus264@live.dk>
2020-04-24 08:53:25 +0200

    Createpolicy Checker

--- SEP2/src/client/view/Salesman/CreatePolice/CreatePoliceViewController.java
+++ SEP2/src/client/view/Salesman/CreatePolice/CreatePoliceViewController.java
@@ -8,12 +8,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+
+import java.util.regex.Pattern;
 
 public class CreatePoliceViewController implements viewController
 {
-@FXML
+  @FXML
+  private Label LabelErrorPrice;
+  @FXML
+  private Label LabelErrorDeductible;
+  @FXML
+  private Label LabelErrorCoverage;
+  @FXML
+  private Label LabelErrorPolicyNr;
+  @FXML
 private ChoiceBox policyType;
 @FXML
  private TextField policyNr;
@@ -39,6 +51,26 @@ private ChoiceBox policyType;
           "Accidents"
       );
 
+  public Label getLabelErrorPrice()
+  {
+    return LabelErrorPrice;
+  }
+
+  public Label getLabelErrorDeductible()
+  {
+    return LabelErrorDeductible;
+  }
+
+  public Label getLabelErrorCoverage()
+  {
+    return LabelErrorCoverage;
+  }
+
+  public Label getLabelErrorPolicyNr()
+  {
+    return LabelErrorPolicyNr;
+  }
+
   @Override public void init(ViewHandler vh, ViewModelFactory vmf, Region root)
   {
        this.vh = vh;
@@ -49,6 +81,10 @@ private ChoiceBox policyType;
        deductible.textProperty().bindBidirectional(cpv.deductibleProperty());
        coverage.textProperty().bindBidirectional(cpv.coverageProperty());
        Customer.textProperty().bindBidirectional(cpv.customerProperty());
+       LabelErrorPolicyNr.textProperty().bind(cpv.labelPolicyNrProperty());
+       LabelErrorPrice.textProperty().bind(cpv.labelPriceProperty());
+       LabelErrorDeductible.textProperty().bind(cpv.labelDeductibleProperty());
+       LabelErrorCoverage.textProperty().bind(cpv.labelCoverageProperty());
 
   policyType.setItems(options);
   }
@@ -57,14 +93,33 @@ private ChoiceBox policyType;
     public Region getRoot() {
         return root;
     }
-
+    public void clearAll()
+    {
+      coverage.clear();
+      price.clear();
+      deductible.clear();
+      Customer.clear();
+      policyNr.clear();
+      LabelErrorPolicyNr.setText("");
+      LabelErrorCoverage.setText("");
+      LabelErrorDeductible.setText("");
+      LabelErrorPrice.setText("");
+    }
     public void onBack()
     {
+      clearAll();
       vh.openView("Policies");
     }
     public void onCreate()
     {
-      cpv.createPolicy((String) policyType.getValue());
-      vh.openView("Policies");
+
+if (cpv.checker() == true)
+{
+
+  cpv.createPolicy((String) policyType.getValue());
+  clearAll();
+  vh.openView("Policies");
+}
+
     }
 }
--- SEP2/src/client/view/Salesman/CreatePolice/CreatePoliceViewModel.java
+++ SEP2/src/client/view/Salesman/CreatePolice/CreatePoliceViewModel.java
@@ -9,16 +9,23 @@ import java.beans.PropertyChangeListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.regex.Pattern;
 
 public class CreatePoliceViewModel implements PropertyChangeListener
 {
   private Model model;
+  CreatePoliceViewController controller;
   private StringProperty policeType;
   private StringProperty policyNr;
   private StringProperty price;
   private StringProperty deductible;
   private StringProperty coverage;
   private StringProperty customer;
+  private StringProperty LabelPolicyNr;
+  private StringProperty LabelPrice;
+  private StringProperty LabelDeductible;
+  private StringProperty LabelCoverage;
+
   private Police police;
 
 
@@ -32,7 +39,11 @@ public class CreatePoliceViewModel implements PropertyChangeListener
     deductible = new SimpleStringProperty();
     coverage = new SimpleStringProperty();
     customer = new SimpleStringProperty();
-
+    controller = new CreatePoliceViewController();
+    LabelPolicyNr = new SimpleStringProperty();
+    LabelPrice = new SimpleStringProperty();
+    LabelDeductible = new SimpleStringProperty();
+    LabelCoverage = new SimpleStringProperty();
   }
 public void createPolicy(String policeType)
 {
@@ -70,6 +81,39 @@ public void saveToDB()
   }
 }
 
+public boolean checker()
+{
+  clear();
+  if (policyNr.getValue() == null || Pattern.matches("[a-åA-å]+", policyNr.getValue()))
+  {
+    LabelPolicyNr.setValue("Missing Policy Nr or invalid input");
+    return false;
+  }
+
+  else if (price.getValue() == null || Pattern.matches("[a-åA-å]+", price.getValue()))
+  {
+   LabelPrice.setValue("Missing Price or invalid input");
+   return false;
+  }
+  else if (deductible.getValue() == null || Pattern.matches("[a-åA-å]+", deductible.getValue()))
+  {
+    LabelDeductible.setValue("Missing Deductible or invalid input");
+    return false;
+  }
+  else if (coverage.getValue() == null || !Pattern.matches("[a-åA-å]+", coverage.getValue()))
+  {
+  LabelCoverage.setValue("Missing Coverage or invalid input");
+  return false;
+  }
+  return true;
+}
+public void clear()
+{
+  LabelCoverage.setValue("");
+  LabelDeductible.setValue("");
+  LabelPolicyNr.setValue("");
+  LabelPrice.setValue("");
+}
   public StringProperty coverageProperty()
   {
     return coverage;
@@ -103,6 +147,28 @@ public void saveToDB()
   public StringProperty deductibleProperty()
   {
     return deductible;
+  }
+
+  public StringProperty labelPolicyNrProperty()
+  {
+    return LabelPolicyNr;
+  }
+
+  public StringProperty labelPriceProperty()
+  {
+    return LabelPrice;
+  }
+
+
+  public StringProperty labelDeductibleProperty()
+  {
+    return LabelDeductible;
+  }
+
+
+  public StringProperty labelCoverageProperty()
+  {
+    return LabelCoverage;
   }
 
   @Override public void propertyChange(PropertyChangeEvent evt)
